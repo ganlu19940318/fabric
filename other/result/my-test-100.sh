58429695246dd85681b835fd4438d9ac5dd7598b
ganlu <274671731@qq.com>
2018-01-18 18:25:40 +0800

    no message

---
+++ other/result/my-test-100.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+demoFun(){
+	local starttime=$(date +%T)
+	peer chaincode invoke -o orderer.example.com:7050  --tls $CORE_PEER_TLS_ENABLED --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C $CHANNEL_NAME -n mycc -c '{"Args":["invoke","a","b","10"]}' 1>/dev/null
+	local endtime=$(date +%T)
+	echo $starttime $endtime $i >> result.txt
+}
+
+start=$(date +%T)
+for (( i = 0; i < 99; ++i ))
+do
+	demoFun & 
+done
+demoFun
+end=$(date +%T)
+echo $start $end "-1">> result.txt
